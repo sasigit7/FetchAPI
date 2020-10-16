af9dcd91b2314f69d7bb3e5be0c902ddb67ee7cc
shashidar mandalozu <sasitech18@gmail.com>
2020-10-16 23:41:52 +0100

    JSON Basics

---
+++ 02.JSONBasics/json.js
@@ -0,0 +1,11 @@
+// JavaScript Object
+const person = {
+    name: "Shashi",
+    age: 35,
+    isMarried: true
+}
+
+// Convert above JavaScript object into a JSON 
+const json = JSON.stringify(person);
+console.log(json); // {"name":"Shashi","age":35,"isMarried":true}
+
