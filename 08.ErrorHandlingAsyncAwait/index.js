5eec7167e0a4bceb660c7d83a104a4e3edf03d45
shashidar mandalozu <sasitech18@gmail.com>
2020-10-17 09:33:56 +0100

    Error Handling Using Async & Await

---
+++ 08.ErrorHandlingAsyncAwait/index.js
@@ -0,0 +1,27 @@
+const getPosts = async () => {
+  const postsPromise = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+if(!postsPromise.ok) {
+  const posts = await postsPromise.json();
+  let html = "";
+
+  posts.slice(0, 5).forEach( post => {
+      const title = post.title;
+      const body = post.body;
+
+      html += `
+            <div class='post'>
+              <h3>${title}</h3>
+              <p>${body}</p>
+            </div>
+        `;
+   });
+  document.body.innerHTML = html;
+} else {
+  console.error(`Error: ${postsPromise.status}`);
+}
+}
+getPosts(); // Error: 200
+
+
+
