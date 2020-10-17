162b034b7a720717194c68711c46c611301445b4
shashidar mandalozu <sasitech18@gmail.com>
2020-10-17 12:50:31 +0100

    Returning a Promise Using finally() Method

---
+++ 15.Promise-FinallyMethod/index.js
@@ -0,0 +1,24 @@
+const promise = () => {
+    return new Promise((resolve, reject) => {
+        reject(new Error('Something went wrong'));
+    })
+}
+
+const resolver = async (promise) => {
+  try {
+      await promise();
+    } catch(err) {
+        console.log(err);
+    } finally {
+      return 'I still return the value though';
+      console.log('I am here');
+    }
+}
+
+(async function() {
+    const test = await resolver(promise);
+    console.log(test);
+})();
+
+
+
